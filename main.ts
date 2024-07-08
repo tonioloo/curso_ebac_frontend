d44ff3ea9f4cfb8d6158bee08844731da463b12c
Gabriel Toniolo <125101739+tonioloo@users.noreply.github.com>
2024-07-08 08:56:32 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+// Função para multiplicar
+
+function multiplicar (a: number, b: number): number {
+  return a * b;
+}
+
+// Função para saudação
+
+function DizOi(nome: string): string {
+  return "Olá " + nome;
+}
+
+// Chamar as funções
+
+console.log(multiplicar(2,3));
+console.log(DizOi("Gabriel"));
